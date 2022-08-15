e3de917267c8fd46abb29a3ef67ec3f2e933d6db
Jorge L. Martinez <jorelmb@gmail.com>
2022-08-15 17:03:00 +0200

    Add agent related information and integration to WandB

---
+++ AbstractAgent.py
@@ -0,0 +1,100 @@
+import random
+from abc import abstractmethod
+from os import urandom
+
+import numpy as np
+import torch
+
+
+class AbstractAgent:
+    def __init__(self, state_size, action_size, *, gamma=1.0, alpha=0.1, seed=-1, **kwargs) -> None:
+        """ Initializes the Agent with environment information and hyperparameters
+
+        Args:
+            space_size (int): Size of the state space
+            action_size (int): Size of the action space
+            gamma (float, optional): Discount rate. Defaults to 1.0.
+            alpha (float, optional): Learning rate. Defaults to 0.1.
+            seed (int, optional): Seed. If -1 then a random seed is used.
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+
+        self.gamma = gamma
+        self.alpha = alpha
+
+        self.epsilon = 1.0
+        self.epsilon_min = 0.05
+
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        # self.device = "cpu"
+
+        if(seed != -1):
+            random.seed(seed)
+        else:
+            seed = int.from_bytes(urandom(4), byteorder="little")
+            random.seed(seed)
+        self.seed = seed
+
+
+    @abstractmethod
+    def start(self, state):
+        """Marks the start of an episode with the initial state. Returns the initial action.
+
+        Args:
+            state (array_like): Initial state
+
+        Returns:
+            int: Initial action
+        """
+        pass
+
+    @abstractmethod
+    def step(self, reward, state, learn=True):
+        """Performs a step in the simulation, provides the reward of the last action and the next state.
+
+        Args:
+            reward (float): Reward from previous action
+            state (array_like): New state
+
+        Returns:
+            int: Next action
+        """
+        pass
+
+    @abstractmethod
+    def end(self, reward):
+        """Finishes an episode, provides the last reward that was provided.
+
+        Args:
+            reward (float): Reward from previous action
+        """
+        pass
+
+    @abstractmethod
+    def __getstate__(self):
+        """Return a state for pickling
+        """
+        pass
+
+    @abstractmethod
+    def __setstate__(self, state):
+        """Restores the instance attributes from a state
+
+        Args:
+            state (dict): Dictionary of elements
+        """
+        pass
+
+    def agent_name(self):
+        return self.__class__.__name__
+
+
+class RandomAgent(AbstractAgent):
+    """ Basic random agent
+    """
+    def __init__(self, state_size, action_size, *, gamma=1, alpha=0.1, seed=-1, **kwargs) -> None:
+        super().__init__(state_size, action_size, gamma=gamma, alpha=alpha, seed=seed, **kwargs)
+    
+    def step(self, reward, state, learn=True):
+        return np.random.uniform(-1, 1, self.action_size)
---
+++ train.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import time
+from collections import deque
+
+import numpy as np
+import torch
+from unityagents import UnityEnvironment
+
+import wandb
+from AbstractAgent import AbstractAgent, RandomAgent
+
+
+def do_episode(environment, agent):
+    """Performs a single episode using the given environment and agent
+
+    Args:
+        environment (env): Environment that will perform the simulation
+        agent (Agent): Agent that will traverse the environment
+
+    Returns:
+        (float, int): Total score and steps of the episode
+    """
+    episode_score = 0
+    env_info = environment.reset(train_mode=True)[brain_name]
+
+    # Start the agent
+    state = env_info.vector_observations[0]
+    next_action = agent.start(state)
+
+    # Take the first action
+    env_info = environment.step(next_action)[brain_name]
+
+    while not env_info.local_done[0]:
+        # Take a step from the agent
+        reward = env_info.rewards[0]
+        episode_score += reward
+        state = env_info.vector_observations[0]
+
+        next_action = agent.step(reward, state)
+
+        # Perform action
+        env_info = environment.step(next_action)[brain_name]
+    
+    # Register last reward to the agent
+    reward = env_info.rewards[0]
+    episode_score += reward
+    agent.end(reward)
+
+    return episode_score
+
+
+def create_agent(state_space, action_space, **kwargs):
+    """Create the list of agents to test
+
+    Returns:
+        list: List of agents
+    """
+    agent = kwargs.get('agent')
+    return RandomAgent(state_space, action_space, **kwargs)
+    
+
+    
+def do_experiment(environment, agent:AbstractAgent, episodes:int, print_every:int):
+    """Performs an experiment on the given agent.
+
+    Args:
+        environment (any): Environment to use
+        agent (AbstractAgent): Agent that follows the "Agent" interface
+        episodes (int): Amount of episodes to perform
+        print_every (int): How often to print the episode information
+
+    Returns:
+        (array_like, array_like): Scores and times that the agent took per episode
+    """
+    scores = np.zeros(episodes)
+    times = np.zeros(episodes)
+
+    for i in range(episodes):
+        start_time = time.time()
+        scores[i] = do_episode(environment, agent)
+        times[i] = time.time() - start_time
+
+        # Log data
+        ep = i+1
+        wandb.log({
+            "episode": ep,
+            "score": scores[i],
+            "time": times[i]
+        })
+        
+        if ep % print_every == 0:
+            print(f"{agent.agent_name()} :: ({ep}/{episodes}) AVG {np.average(scores[max(0, i-print_every):])}")
+    
+    return scores, times
+
+if __name__ == '__main__':
+    # Load environment and get initial brain
+    env = UnityEnvironment(file_name='ReacherOne/Reacher.x86_64', no_graphics=True)
+    brain_name = env.brain_names[0]
+    brain = env.brains[brain_name]
+
+    # Initialize environment for use of the agent
+    env_info = env.reset(train_mode=True)[brain_name]
+    action_space = brain.vector_action_space_size
+    state_space = env_info.vector_observations.size
+
+    with wandb.init(project='nanorl-p2', entity='jorelmb') as run:
+        episodes = wandb.config.episodes
+        print_every = 100
+
+        agent = create_agent(state_space, action_space, **wandb.config)
+        scores, times = do_experiment(env, agent, episodes, print_every)
+        agent_name = agent.agent_name()
+
+        # Save agent
+        torch.save(agent, f"agents/{agent_name}-{run.id}.pt")
+
+    # Close environment
+    env.close()
+
