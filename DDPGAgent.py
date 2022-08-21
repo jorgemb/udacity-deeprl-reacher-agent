57bdd1a721a3b63e084e0a20adfb641c23aca803
Jorge L. Martinez <jorelmb@gmail.com>
2022-08-21 21:03:06 +0200

    Using deep-rl implementation.

--- DDPGAgent.py
+++ DDPGAgent.py
@@ -1,34 +1,84 @@
 from turtle import forward
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 
+import random_process
 import rllib.ReplayBuffer
+import schedule
 from AbstractAgent import AbstractAgent
 
 
-class LinearModel(nn.Module):
-    def __init__(self, state_space, action_space, hidden=(32, 32), activation_fn: any = nn.ReLU,
-                 end_activation_fn: any = nn.ReLU) -> None:
-        super().__init__()
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return -lim, lim
 
-        self.layers = nn.Sequential()
 
-        # Create hidden layers
-        last_size = state_space
-        for i, val in enumerate(hidden):
-            self.layers.add_module(f'fc{i}', nn.Linear(last_size, val))
-            self.layers.add_module(f'act{i}', activation_fn())
-            last_size = val
+class Actor(nn.Module):
+    """Actor (Policy) Model."""
 
-            # Add output layer
-            if i == len(hidden) - 1:
-                self.layers.add_module(f'fc_end', nn.Linear(last_size, action_space))
-                if end_activation_fn is not None:
-                    self.layers.add_module(f'act_end', end_activation_fn())
+    def __init__(self, state_size, action_size, seed, fc_units=256):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(Actor, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc_units)
+        self.fc2 = nn.Linear(fc_units, action_size)
+        self.reset_parameters()
 
-    def forward(self, x):
-        return self.layers.forward(x)
+    def reset_parameters(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, state):
+        """Build an actor (policy) network that maps states -> actions."""
+        x = F.relu(self.fc1(state))
+        return F.tanh(self.fc2(x))
+
+
+class Critic(nn.Module):
+    """Critic (Value) Model."""
+
+    def __init__(self, state_size, action_size, seed, fcs1_units=256, fc2_units=256, fc3_units=128):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fcs1_units (int): Number of nodes in the first hidden layer
+            fc2_units (int): Number of nodes in the second hidden layer
+        """
+        super(Critic, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fcs1 = nn.Linear(state_size, fcs1_units)
+        self.fc2 = nn.Linear(fcs1_units + action_size, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, fc3_units)
+        self.fc4 = nn.Linear(fc3_units, 1)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
+        self.fc4.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, state, action):
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        xs = F.leaky_relu(self.fcs1(state))
+        x = torch.cat((xs, action), dim=1)
+        x = F.leaky_relu(self.fc2(x))
+        x = F.leaky_relu(self.fc3(x))
+        return self.fc4(x)
 
 
 class DDPGAgent(AbstractAgent):
@@ -36,14 +86,20 @@ class DDPGAgent(AbstractAgent):
         super().__init__(state_size, action_size, gamma=gamma, alpha=alpha, seed=seed, **kwargs)
 
         # Create actor-critic networks
-        self.local_actor = LinearModel(state_size, action_size, end_activation_fn=nn.Tanh).to(self.device)
-        self.target_actor = LinearModel(state_size, action_size, end_activation_fn=nn.Tanh).to(self.device)
-
-        self.local_critic = LinearModel(state_size + action_size, 1).to(self.device)
-        self.target_critic = LinearModel(state_size + action_size, 1).to(self.device)
-
+        # Actor
+        self.local_actor = Actor(state_size, action_size, self.seed).to(self.device)
+        self.target_actor = Actor(state_size, action_size, self.seed).to(self.device)
         self.actor_optimizer = torch.optim.Adam(self.local_actor.parameters(), lr=self.alpha)
+
+        # Critic
+        self.local_critic = Critic(state_size, action_size, self.seed).to(self.device)
+        self.target_critic = Critic(state_size, action_size, self.seed).to(self.device)
         self.critic_optimizer = torch.optim.Adam(self.local_critic.parameters(), lr=self.alpha)
+
+        self.random_process = random_process.OrnsteinUhlenbeckProcess(
+            size=(self.action_size,),
+            std=schedule.LinearSchedule(0.2)
+        )
 
         self.last_state = None
         self.last_action = None
@@ -62,18 +118,23 @@ class DDPGAgent(AbstractAgent):
             device=self.device
         )
 
-    def take_action(self, state: np.array, do_random: bool = False):
+    def take_action(self, state: np.array, add_noise: bool = True):
         """
         Takes an action using the local actor or a random value
         @param state:
-        @param do_random:
+        @param add_noise:
         @return:
         """
-        if do_random:
-            return np.random.uniform(-1, 1, self.action_size)
-        else:
-            x = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
-            return self.target_actor.forward(x).cpu().data.numpy()
+        # Get action without training
+        s = torch.from_numpy(state).float().to(self.device)
+        self.local_actor.eval()
+        with torch.no_grad():
+            action = self.local_actor(s).cpu().data.numpy()
+        self.local_actor.train()
+
+        if add_noise:
+            action += self.random_process.sample()
+        return np.clip(action, -1, 1)
 
     def start(self, state):
         self.step_n = 1
@@ -90,7 +151,7 @@ class DDPGAgent(AbstractAgent):
 
         # Get next action
         self.last_state = next_state
-        self.last_action = self.take_action(state)
+        self.last_action = self.take_action(state, add_noise=True)
         self.step_n += 1
 
         # Check if we should do a learning step
@@ -100,27 +161,31 @@ class DDPGAgent(AbstractAgent):
         return self.last_action
 
     def learn_step(self):
+        # Get sample
         states, actions, rewards, next_states, dones = self.replay.sample()
 
-        # Forward pass with target networks
-        a_next = self.target_actor.forward(next_states)
-        q_next = self.target_critic.forward(torch.cat([next_states, a_next], dim=1))
-        q_next = self.gamma * dones * q_next + rewards
-        q_next = q_next.detach()
+        # Update critic
+        a_next = self.target_actor(next_states)
+        q_target = self.target_critic(next_states, a_next)
+        q_target = self.gamma * (1 - dones) * q_target + rewards
+        q_target = q_target.detach()
 
-        # Pass with local networks
-        q = self.local_critic(torch.cat([states, actions], dim=1))
-        critic_loss = (q - q_next).pow(2).mul(0.5).sum(-1).mean()
+        # .. compute expected
+        q_expected = self.local_critic(states, actions)
+        critic_loss = F.mse_loss(q_expected, q_target)
 
+        # .. step with optimizer
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         self.critic_optimizer.step()
 
-        next_actions = self.local_actor.forward(states)
-        policy_loss = -self.local_critic(torch.cat([states.detach(), next_actions], dim=1)).mean()
+        # Update actor
+        a_predicted = self.local_actor(states)
+        actor_loss = -self.local_critic(states, a_predicted).mean()
 
+        # .. step with optimizer
         self.actor_optimizer.zero_grad()
-        policy_loss.backward()
+        actor_loss.backward()
         self.actor_optimizer.step()
 
         # Soft update the networks
